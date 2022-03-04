af546c622e1df120f690940e3386ac3e9016781b
Evgeniy <denisov.evgeniy@gmail.com>
2022-03-04 11:38:10 +0300

    Create entername.py

---
+++ messenger/entername.py
@@ -0,0 +1,43 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_NameWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(189, 178)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.nameEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.nameEdit.setObjectName("nameEdit")
+        self.verticalLayout.addWidget(self.nameEdit)
+        self.nextbtn = QtWidgets.QPushButton(self.centralwidget)
+        self.nextbtn.setObjectName("nextbtn")
+        self.verticalLayout.addWidget(self.nextbtn)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 189, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Введите имя:"))
+        self.nextbtn.setText(_translate("MainWindow", ">"))
